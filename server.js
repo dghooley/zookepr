809892d1474e1fee082ea9d733c6ebb7f7270ff3
Derick Hooley <dghooley@gmail.com>
2021-10-14 12:48:17 -0500

    codeSnapshot

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
